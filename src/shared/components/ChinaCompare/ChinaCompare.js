5f9ddebdb0a2937aca2401808b4908538704a678
user <user@user>
2017-01-19 15:19:38 +0100

    update

--- src/shared/components/ChinaCompare/ChinaCompare.js
+++ src/shared/components/ChinaCompare/ChinaCompare.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import React from 'react';
+import React, { Component } from 'react';
 import { Match, Miss } from 'react-router';
 import Helmet from 'react-helmet';
 import { CodeSplit } from 'code-split-component';
@@ -12,51 +12,53 @@ import './globals.scss';
 import Error404 from './Error404';
 import Header from './Header';
 
-function ChinaCompare() {
-  return (
-    <div className="pusher">
-      {/*
-       All of the following will be injected into our page header.
-       @see https://github.com/nfl/react-helmet
-       */}
-      <Helmet
-        htmlAttributes={safeConfigGet(['htmlPage', 'htmlAttributes'])}
-        titleTemplate={safeConfigGet(['htmlPage', 'titleTemplate'])}
-        defaultTitle={safeConfigGet(['htmlPage', 'defaultTitle'])}
-        meta={safeConfigGet(['htmlPage', 'meta'])}
-        link={safeConfigGet(['htmlPage', 'links'])}
-        script={safeConfigGet(['htmlPage', 'scripts'])}
-      />
-      <Header />
-      <div className="ui vertical stripe segment">
-        <div className="ui middle aligned stackable grid container">
-          <div className="row">
+export class ChinaCompare extends Component {
+  render() {
+    return (
+      <div className="pusher">
+        {/*
+         All of the following will be injected into our page header.
+         @see https://github.com/nfl/react-helmet
+         */}
+        <Helmet
+          htmlAttributes={safeConfigGet(['htmlPage', 'htmlAttributes'])}
+          titleTemplate={safeConfigGet(['htmlPage', 'titleTemplate'])}
+          defaultTitle={safeConfigGet(['htmlPage', 'defaultTitle'])}
+          meta={safeConfigGet(['htmlPage', 'meta'])}
+          link={safeConfigGet(['htmlPage', 'links'])}
+          script={safeConfigGet(['htmlPage', 'scripts'])}
+        />
+        <Header />
+        <div className="ui vertical stripe segment">
+          <div className="ui middle aligned stackable grid container">
+            <div className="row">
 
-            <Match
-              exactly
-              pattern="/"
-              render={routerProps =>
+              <Match
+                exactly
+                pattern="/"
+                render={routerProps =>
           <CodeSplit chunkName="home" modules={{ Home: require('./Home') }}>
             { ({ Home }) => Home && <Home {...routerProps} /> }
           </CodeSplit>
               }
-            />
+              />
 
-            <Match
-              pattern="/about"
-              render={routerProps =>
+              <Match
+                pattern="/about"
+                render={routerProps =>
           <CodeSplit chunkName="about" modules={{ About: require('./About') }}>
             { ({ About }) => About && <About {...routerProps} /> }
           </CodeSplit>
               }
-            />
+              />
 
-            <Miss component={Error404}/>
+              <Miss component={Error404}/>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ChinaCompare;
